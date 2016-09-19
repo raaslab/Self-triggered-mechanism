f4a235d633dceb90f1780cd604906aa39c527377
lovetuliper <lovetuliper@gmail.com>
2016-09-19 12:27:30 -0400

    positiontoangularfun updated

--- positiontoangularfun.m
+++ positiontoangularfun.m
@@ -1,7 +1,11 @@
+%function[angular]=positiontoangularfun(y,x,baseangular,pasty,pastx)
 function[angular]=positiontoangularfun(y,x)
-if(atan2(y,x)<0)
-    angular=atan2d(y,x)+360;
-else
-    angular=atan2d(y,x);
-end
+%angular=baseangular+(atan2d(y,x)-atan2d(pasty,pastx));
+
+angular=atan2d(y,x);
+% if(atan2(y,x)<0)
+%     angular=atan2d(y,x)+360;
+% else
+%     angular=atan2d(y,x);
+% end
 end
